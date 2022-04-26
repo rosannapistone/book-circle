4dbd94e2a587d55c5abc7095a5c02f830cb17883
elinarnten <elin.arnten@hotmail.com>
2022-04-26 17:39:42 +0200

    litle cookies

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
-
 import mongoose from "mongoose";
 import userModel from "./models/user.model.js";
-import "./connect.js"
-import express from "express"
-import cors from "cors"
-import booksRouter from "./routes/books.js"
-import usersRouter from "./routes/user.js"
-//import cookieSession from "cookie-session"
-import cookieParser from ('cookie-parser')
+import "./connect.js";
+import express from "express";
+import cors from "cors";
+import booksRouter from "./routes/books.js";
+import usersRouter from "./routes/user.js";
+import cookieSession from "cookie-session";
+// import cookieParser from "cookie-parser";
 
+const app = express();
+app.use(express.json());
+// app.use(cookieParser());
+app.use("/", express.static("public"));
 
-/* app.use(
+app.use(cors({ credentials: true, origin: ["http://localhost:3000"] }));
+app.use(express.static("public"));
+app.use(
   cookieSession({
     name: "session",
     keys: "aVeryS3cr3tk3y",
@@ -19,21 +24,39 @@ import cookieParser from ('cookie-parser')
     httpOnly: true,
     secure: false,
   })
-);  */
+);
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use("/", express.static("public"));
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
 
+app.post("/login", async (req, res) => {
+  // Check if username and password is correct
+  const user = users.find((user) => user.name === req.body.name);
+  if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+    return res.status(401).send("Wrong password or username");
+  }
 
-app.use(cors({ credentials: true, origin: ["http://localhost:3000"] }));
-app.use(express.static("public"));
+  // Check if user aleady is logged in
+  if (req.session.id) {
+    return res.send("Already logged in");
+  }
 
+  // Save info about the user to the session (a cookie stored on the clinet)
+  req.session.id = uuid.v4();
+  req.session.username = user.name;
+  req.session.loginDate = new Date();
+  req.session.role = undefined; // User could have a role (access privileges)
+  res.send("Successful login");
+});
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World' });
-  });
+app.delete("/logout", (req, res) => {
+  if (!req.session.id) {
+    return res.status(400).send("Cannot logout when you are not logged in");
+  }
+  req.session = null;
+  res.send("Your are now logged out");
+});
 
 // Routes for users
 app.use("/users", usersRouter);
@@ -43,4 +66,3 @@ app.use("/books", booksRouter);
 
 // Server running
 app.listen(3001, () => console.log("Listening at 3001"));
-
